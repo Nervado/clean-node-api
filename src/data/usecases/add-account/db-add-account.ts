a0aa15781d763121a1e08be003004cbb46594b3f
Nervado <evandroabreudeoliveira@gmail.com>
2021-02-12 18:50:43 -0300

    feat: ensure DbAddAccount return an accoun on sucess

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -5,12 +5,7 @@ export class DbAddAccount implements AddAccount {
   constructor (private readonly encrypter: Encrypter, private readonly addAccountRepository: AddAccountRepository) { }
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
-    return await new Promise(resolve => resolve({
-      id: '',
-      name: '',
-      email: '',
-      password: ''
-    }))
+    const account = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
+    return account
   }
 }
