e528d46569290e4938426f36ba9218d7bc1f52d0
Nervado <evandroabreudeoliveira@gmail.com>
2021-02-13 12:03:08 -0300

    refactor: change param name of map function

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -20,8 +20,8 @@ export const MongoHelper = {
     return this.client.db().collection(name)
   },
 
-  map (data: any): any {
-    const { _id, ...args } = data
+  map (collection: any): any {
+    const { _id, ...args } = collection
     return { id: _id, ...args }
   }
 }
