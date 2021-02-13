e29bd22354dcf4ceada1fad1ca8a40ffb6c1a758
Nervado <evandroabreudeoliveira@gmail.com>
2021-02-13 11:51:30 -0300

    refactor: add map function to mongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: typeof MongoClient,
@@ -14,5 +14,14 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map (data: any): any {
+    const { _id, ...args } = data
+    return { id: _id, ...args }
   }
 }
