8f4b4c995f3164a49b17715669c6474d81204733
Nervado <evandroabreudeoliveira@gmail.com>
2021-02-13 11:59:55 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,6 +11,10 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
   const makeSut = (): AddAccountRepository => new AccountMongoRepository()
 
   test('Should return an account on success', async () => {
