31fe082600c62f164d747ff63ea7426cd082f04c
Nervado <evandroabreudeoliveira@gmail.com>
2021-02-16 14:17:47 -0300

    test: ensure LogControllerDecorator return the same result of the Controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -38,4 +38,21 @@ describe('LogControllerDecorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toBeCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest: HttpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {}
+    })
+  })
 })
